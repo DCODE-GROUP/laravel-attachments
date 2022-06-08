b35a689a66cf72ae24272f0aeb1af0866e24961b
Robert Rimoczi <robert@rimoczi.com>
2022-06-08 12:07:15 +0200

    init

---
+++ src/LaravelAttachmentsServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Dcodegroup\LaravelAttachments;
+
+use Closure;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelAttachmentsServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+    }
+}
