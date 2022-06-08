81d905021b1346c1d7f03046a2827e8cb8f0f373
Robert Rimoczi <robert@rimoczi.com>
2022-06-08 12:23:53 +0200

    add route folder, fix up publishing and loading

--- src/LaravelAttachmentsServiceProvider.php
+++ src/LaravelAttachmentsServiceProvider.php
@@ -17,14 +17,31 @@ class LaravelAttachmentsServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->setUpConfig();
+        $this->setUpRouting();
+        $this->setUpTranslations();
         $this->setUpMigrations();
+        $this->setUpViews();
     }
 
     protected function setUpConfig()
     {
         $this->publishes([
-            __DIR__.'/../config/attachments.php' => config_path('attachments.php'),
-        ]);
+            __DIR__.'/../config/attachments.php' => $this->app->configPath('attachments.php'),
+        ], 'laravel-attachments-config');
+    }
+
+    protected function setUpRouting()
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/attachments.php');
+    }
+
+    protected function setUpTranslations()
+    {
+        $this->loadTranslationsFrom(__DIR__.'/../lang', 'attachments');
+
+        $this->publishes([
+            __DIR__.'/../lang' => $this->app->langPath('vendor/attachments'),
+        ], 'laravel-attachments-translations');
     }
 
     protected function setUpMigrations()
@@ -32,7 +49,16 @@ class LaravelAttachmentsServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
         $this->publishes([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
+            __DIR__.'/../database/migrations' => $this->app->databasePath('migrations'),
         ], 'laravel-attachments-migrations');
+    }
+
+    protected function setUpViews()
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'attachments');
+
+        $this->publishes([
+            __DIR__.'/../resources/views' => resource_path('views/vendor/attachments'),
+        ], 'laravel-attachments-views');
     }
 }
