5ffc3a93776b897ac5e3e8d5ce940493734de829
Robert Rimoczi <robert@rimoczi.com>
2022-06-08 12:16:53 +0200

    add annotation and category

---
+++ src/Models/Annotation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dcodegroup\LaravelAttachments\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Annotation extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var string[]|bool
+     */
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'hidden' => 'bool',
+    ];
+
+    public function media(): BelongsTo
+    {
+        return $this->belongsTo(Media::class);
+    }
+}
---
+++ src/Models/Category.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Dcodegroup\LaravelAttachments\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    public const TYPE_ATTACHMENT = 'attachment';
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    public function children(): HasMany
+    {
+        return $this->hasMany(self::class, 'parent_id')->orderBy('name');
+    }
+
+    public function childrenRecursive(): HasMany
+    {
+        return $this->children()->with('childrenRecursive');
+    }
+
+    /**
+     * @return mixed
+     */
+    public static function attachmentCategories()
+    {
+        return self::query()->byType(self::TYPE_ATTACHMENT)->get();
+    }
+
+    public function scopeByType(Builder $query, string $type): Builder
+    {
+        return $query->where('type', $type);
+    }
+
+    public function scopeForParentId(Builder $query, int $parentId): Builder
+    {
+        return $query->where('parent_id', $parentId);
+    }
+}
