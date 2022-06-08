43911c4cb1df4323a02e06cdbdb9a54a3b462376
Robert Rimoczi <robert@rimoczi.com>
2022-06-08 16:55:27 +0200

    add migrations

---
+++ database/migrations/2021_12_01_153101_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('parent_id')->nullable();
+
+            $table->string('name');
+            $table->string('type');
+            $table->foreignId('user_id')->nullable()->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->index('parent_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
---
+++ database/migrations/2022_02_02_080557_add_parent_id_to_media_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->unsignedBigInteger('parent_id')->after('id')->nullable();
+            $table->string('model_type')->nullable()->change();
+            $table->unsignedBigInteger('model_id')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->dropColumn('parent_id');
+        });
+    }
+};
