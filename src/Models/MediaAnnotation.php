73e274845b646e8f2a5d046c5eeb2a0a9461e9ea
lionslair <lionslair@users.noreply.github.com>
2024-02-14 07:52:37 +0000

    Fix PHP Syntax Styling

--- src/Models/MediaAnnotation.php
+++ src/Models/MediaAnnotation.php
@@ -2,7 +2,6 @@
 
 namespace Dcodegroup\LaravelAttachments\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
