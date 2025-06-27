1ce01dadaaee8cb1c424c4b137e798b9fb16d398
Nathan Rzepecki <nathan@lionslair.net.au>
2025-06-27 16:00:01 +1000

    wip

--- src/Http/Controllers/Media/DownloadController.php
+++ src/Http/Controllers/Media/DownloadController.php
@@ -11,13 +11,16 @@ class DownloadController
 {
     use AuthorizesRequests;
 
-    public function __invoke(Request $request, Media $media): BinaryFileResponse
+    public function __invoke(Request $request, int $media): BinaryFileResponse
     {
         $this->authorize('download', $media);
 
         //        Storage::disk($media->disk)
         //               ->path($media->getPath()),   Storage::disk($media->disk)
         //               ->path($media->getPath()),
+
+        dd($media);
+        dd(Media::findOrFail($media));
 
         return response()->download(
             file: $media->getPath(),
