c3e8d441c1414ede427ffb1c16f9eacef749baca
Nathan Rzepecki <nathan@lionslair.net.au>
2025-06-30 12:21:39 +0800

    Set the return type of download

--- src/Http/Controllers/Media/DownloadController.php
+++ src/Http/Controllers/Media/DownloadController.php
@@ -6,19 +6,20 @@ use Dcodegroup\LaravelAttachments\Models\Media;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class DownloadController
 {
     use AuthorizesRequests;
 
-    public function __invoke(Request $request, Media $media)
+    public function __invoke(Request $request, Media $media): StreamedResponse
     {
         $this->authorize('download', $media);
 
         return Storage::disk($media->disk)
-                      ->download(
-                          path: $media->getPath(),
-                          name: $media->getCustomProperty('original_filename')
-                      );
+            ->download(
+                path: $media->getPath(),
+                name: $media->getCustomProperty('original_filename')
+            );
     }
 }
